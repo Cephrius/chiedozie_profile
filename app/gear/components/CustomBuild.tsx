6e31a14960d686b9fc7435fa0385c33e9d0028fc
Chiedozie Ehileme <ehilemece@hc.edu>
2024-02-17 03:28:15 -0600

    removed unused array

--- app/gear/components/CustomBuild.tsx
+++ app/gear/components/CustomBuild.tsx
@@ -39,26 +39,6 @@ const App: React.FC<GearProps> = ({ item, description, image, link }: GearProps)
   );
 };
 
-const gear = [
-  {
-    item: "Intel I7-13700K",
-    description: "CPU",
-    image: "https://m.media-amazon.com/images/I/61m0zH-NiTL._AC_SL1500_.jpg",
-    link: "https://www.amazon.com/Intel-i7-13700K-Desktop-Processor-P-cores/dp/B0BCF57FL5/ref=sr_1_1?crid=2X94ZQMMFN99D&keywords=i7-13700k&qid=1691720014&sprefix=i7-13700k%2Caps%2C223&sr=8-1&ufe=app_do%3Aamzn1.fos.c3015c4a-46bb-44b9-81a4-dc28e6d374b3",
-  },
-  {
-    item: "Intel I7-13700K",
-    description: "CPU",
-    image: "https://m.media-amazon.com/images/I/61m0zH-NiTL._AC_SL1500_.jpg",
-    link: "https://www.amazon.com/Intel-i7-13700K-Desktop-Processor-P-cores/dp/B0BCF57FL5/ref=sr_1_1?crid=2X94ZQMMFN99D&keywords=i7-13700k&qid=1691720014&sprefix=i7-13700k%2Caps%2C223&sr=8-1&ufe=app_do%3Aamzn1.fos.c3015c4a-46bb-44b9-81a4-dc28e6d374b3",
-  },
-  {
-    item: "Intel I7-13700K",
-    description: "CPU",
-    image: "https://m.media-amazon.com/images/I/61m0zH-NiTL._AC_SL1500_.jpg",
-    link: "https://www.amazon.com/Intel-i7-13700K-Desktop-Processor-P-cores/dp/B0BCF57FL5/ref=sr_1_1?crid=2X94ZQMMFN99D&keywords=i7-13700k&qid=1691720014&sprefix=i7-13700k%2Caps%2C223&sr=8-1&ufe=app_do%3Aamzn1.fos.c3015c4a-46bb-44b9-81a4-dc28e6d374b3",
-  },
-];
 
 // Map the gear array to render the respective Card components
 const Custombuild = () => {
