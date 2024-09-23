04ae58b3baf450a225a1a837125316d938eeec81
Chiedozie Ehileme <ehilemece@hc.edu>
2024-09-23 14:34:07 -0500

    fixed description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,10 @@ import clsx from "clsx";
 const inter = Inter({ subsets: ["latin"] });
 
 // Add meta data
-export const metadata: Metadata = {
-  title: "Chiedozie ",
-  description: "Chiedozie Ehileme is a Computer Science Student and Founder of Cephrius Technologies ",
-};
+// export const metadata: Metadata = {
+//   title: "Chiedozie ",
+//   description: "Chiedozie Ehileme is a Computer Science Student and Founder of Cephrius Technologies ",
+// };
 
 
 export default function RootLayout({
